46d70190a26353b1e5bdc287321567d586fd6906
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2022-10-21 18:17:59 +0600

    Create 65.c

---
+++ 65.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+
+    for(i=1; i<=100; i++)
+    {
+        if(i!=49 && i!=51)
+        {
+            printf("%d ",i);
+        }
+
+
+    }
+
+
+
+
+    return 0;
+}
